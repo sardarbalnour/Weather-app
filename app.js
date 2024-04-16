c541fe75a2def5d58cbdf6ab6da59b2eba0bb2ef
Sardar Balnour <balnour.sardar@gmail.com>
2024-04-16 13:32:41 +0330

    showing data

--- app.js
+++ app.js
@@ -4,12 +4,33 @@ const API_KEY = "8c58c7c8f0d2c56f4a39b999df0c2b7d";
 
 const searchInput = document.querySelector("input");
 const searchButton = document.querySelector("button");
+const weatherContainer = document.querySelector("#weather");
 
 const getCurrentWeatherByName = async (city) => {
   const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`;
   const response = await fetch(url);
   const json = await response.json();
   return json;
+};
+
+const renderCurrentWeather = (data) => {
+  console.log(data);
+  const weatherJSX = `
+    <h1>${data.name}, ${data.sys.country}</h1>
+    <div id="main">
+      <img alt="weather icon" src="https://openweathermap.org/img/w/${
+        data.weather[0].icon
+      }.png" />
+      <span>${data.weather[0].main}</span>
+      <p>${Math.round(data.main.temp)} °C</p>
+    </div>
+    <div id="info">
+      <p>Humidity: <span>${data.main.humidity} %</span></p>
+      <p>Wind Speed: <span>${data.wind.speed} m/s</span></p>
+    </div>
+  `;
+
+  weatherContainer.innerHTML = weatherJSX;
 };
 
 const searchHandler = async () => {
@@ -20,7 +41,7 @@ const searchHandler = async () => {
   }
 
   const currentData = await getCurrentWeatherByName(cityName);
-  console.log(currentData);
+  renderCurrentWeather(currentData);
 };
 
 searchButton.addEventListener("click", searchHandler);
